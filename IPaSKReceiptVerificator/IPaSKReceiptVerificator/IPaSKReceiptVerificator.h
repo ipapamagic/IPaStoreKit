91b885b1d3a24e930c0c66fe51bd6c3dd5a043f2
IPa Chen <ipapamagic@gmail.com>
2015-02-24 10:54:19 +0800

    parse integrate

---
+++ IPaSKReceiptVerificator/IPaSKReceiptVerificator/IPaSKReceiptVerificator.h
@@ -0,0 +1,20 @@
+//
+//  IPaSKReceiptVerificator.h
+//  IPaSKReceiptVerificator
+//
+//  Created by IPa Chen on 2015/2/17.
+//  Copyright (c) 2015年 A Magic Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IPaSKReceiptVerificator.
+FOUNDATION_EXPORT double IPaSKReceiptVerificatorVersionNumber;
+
+//! Project version string for IPaSKReceiptVerificator.
+FOUNDATION_EXPORT const unsigned char IPaSKReceiptVerificatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IPaSKReceiptVerificator/PublicHeader.h>
+#import <Foundation/Foundation.h>
+#import "IPaIAPReceipt.h"
+#import "IPaAppReceipt.h"
---
+++ IPaStoreKit/IPaStoreKit/IPaStoreKit.h
@@ -0,0 +1,24 @@
+//
+//  IPaStoreKit.h
+//  IPaStoreKit
+//
+//  Created by IPa Chen on 2015/2/21.
+//  Copyright 2015年 A Magic Studio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+//! Project version number for IPaStoreKit.
+FOUNDATION_EXPORT double IPaStoreKitVersionNumber;
+
+//! Project version string for IPaStoreKit.
+FOUNDATION_EXPORT const unsigned char IPaStoreKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IPaStoreKit/PublicHeader.h>
+
+
+
+@interface IPaStoreKit : NSObject {
+    
+}
++ (instancetype)sharedInstance;
+@end
